28c4cc88d416a3e43da3ac793d1124c3ab5f1362
Amir Hossein Kargar <amirkaargar@hotmail.com>
2024-10-15 14:46:43 +0330

    add: deploy file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/course-list-app/",
-});
+  base: "/course-list-app/"
+})
