bc0336a7a3b08cc65d80c6730ee72b661589eb43
Amir Hossein Kargar <152816299+AmirHossein-Kargar@users.noreply.github.com>
2024-10-15 15:06:42 +0330

    Update CourseList.jsx

--- src/Components/CourseList.jsx
+++ src/Components/CourseList.jsx
@@ -4,7 +4,7 @@ const courses = [
     id: 1,
     title: "English Lecture",
     description: "language lessons with the most popular teachers",
-    imageUrl: "/public/images/img1.jpg",
+    imageUrl: "public/images/img1.jpg",
     rate: "4.5",
     tags: ["langauges"],
     start: "2021-08-01T20:46:30.615Z",
@@ -15,7 +15,7 @@ const courses = [
     title: "Design Strategy",
     description:
       "lesson on planning design concept and proper planning of work",
-    imageUrl: "/public/images/img2.jpg",
+    imageUrl: "public/images/img2.jpg",
     rate: "4",
     tags: ["UI/UX design ", "web design"],
     start: "2023-07-01T20:46:30.615Z",
@@ -26,7 +26,7 @@ const courses = [
     title: "Business Lecture",
     description:
       "lectures on how to build your buisness safely without fare of new projects",
-    imageUrl: "/public/images/img3.jpg",
+    imageUrl: "public/images/img3.jpg",
     rate: "3.9",
     tags: ["Marketing ", "Finance"],
     start: "2024-04-01T20:46:30.615Z",
